4d71ef5b51964ab5c1239003d2c79402733ed1b7
Macy Huang <mjmj3794@gmail.com>
2024-08-11 19:01:41 +0800

    新增main 頁面

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
